e9a16a57bfb5d6a024915b31bc0b7f077e9475f2
Alina <AliKochneva@yandex.ru>
2023-04-30 20:09:43 +0700

    установка якрорей

--- js/app.js
+++ js/app.js
@@ -2,9 +2,34 @@
 const BurgerButton = document.querySelector('.burger-menu')
 const BurgerSpan = document.querySelector('.menu-span')
 
-//работа с кнопкой меню
+const intoLink = document.querySelector ('#startWord')
+const gridLink = document.querySelector("#gridLink")
+const typographyLink = document.querySelector('#typographyLink')
+const UILink = document.querySelector('#UIlink')
+
+
+//работа с кнопкой меню (добавление класса active для двух элиментов, которые будут менять своё положение и цвет)
 
 BurgerButton.addEventListener('click', () => {
     BurgerButton.classList.toggle('active')
     BurgerSpan.classList.toggle('active')
+})
+
+//якоря 
+//при клике на одну из ссылок sidebar страница плавно переместить к началу соответствующего блока
+
+intoLink.addEventListener('click', () => {
+    window.scrollTo({ top: 40, behavior: 'smooth' });
+})
+
+gridLink.addEventListener('click', () => {
+    window.scrollTo({ top: 180, behavior: 'smooth' });
+})
+
+typographyLink.addEventListener('click', () => {
+    window.scrollTo({ top: 2500, behavior: 'smooth' })
+})
+
+UI.addEventListener('click', () => {
+    window.scrollTo({ top: 2700, behavior: 'smooth' })
 })
